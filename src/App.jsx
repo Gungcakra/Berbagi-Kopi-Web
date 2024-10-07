69449324590c033ed3169f3352fdf2199d57bd70
Gungcakra <agungcakra888@gmail.com>
2024-10-07 11:06:09 +0800

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/Home'
 import { Tentang } from './components/Tentang'
 import Menu from './components/Menu'
 import Galery from './components/Galery'
+import Footer from './components/Footer'
 function App() {
 
   return (
@@ -12,6 +13,7 @@ function App() {
           <Tentang  />
           <Menu />
           <Galery />
+          <Footer />
        </div>
     </>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/Footer.css"
+function Footer(){
+    return (
+        <>
+            <section className="footer-section container-fluid">
+                <h4 className="text-center">Copyright  &copy; <span>Cakra Jaya</span></h4>
+            </section>
+        </>
+    )
+}
+
+export default Footer;
